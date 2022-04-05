ea5ba34957f31c148c50e7ea955523205a3c8814
Shurel Reynolds <shurel_reynolds@yahoo.com>
2022-04-05 06:32:42 +0100

    Added GridSize

--- src/main/java/com/shurel/tictactoe/TicTacToe.java
+++ src/main/java/com/shurel/tictactoe/TicTacToe.java
@@ -1,4 +1,43 @@
+/*
+@author Shurel Reynolds
+*/
 package com.shurel.tictactoe;
 
+import java.util.Arrays;
+
+
 public class TicTacToe {
+    private int gridSize = 3;
+    //a game has nine fields in a 3x3 grid
+    private String[][] grid = new String[gridSize][gridSize];
+
+    public TicTacToe() {
+        System.out.println("Cross & Naught :D");
+        //fill -  empty
+        for (String i[] : grid) {
+            // Fill each row with -.
+            Arrays.fill(i, "-");
+        }
+        printGrid();
+    }
+
+    public String getGrid() {
+        String out = "";
+        for (String i[] : grid) {
+            for (String j : i) {
+                out += j + " ";
+            }
+            out += "\n";
+
+        }
+        return out;
+    }
+
+    public void printGrid() {
+        System.out.println(getGrid());
+    }
+
+    public int getGridSize() {
+        return gridSize;
+    }
 }
--- src/test/java/TicTacToeTest.java
+++ src/test/java/TicTacToeTest.java
@@ -1,13 +1,24 @@
+/*
+@author Shurel Reynolds
+*/
 import com.shurel.tictactoe.TicTacToe;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 public class TicTacToeTest {
 
-private TicTacToe ticTacToe;
+    private TicTacToe ticTacToe;
 
     @Before
-    public void setUp(){
-       ticTacToe=new TicTacToe();
+    public void setUp() {
+        ticTacToe = new TicTacToe();
+
+    }
+
+    @Test
+    public void testGridSize() {
+        Assert.assertTrue("GridSize == 3", ticTacToe.getGridSize() == 3);
     }
 
 }
